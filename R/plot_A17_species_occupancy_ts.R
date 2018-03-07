8858b88fb73da923a490a6a72ea1eaa95a6347be
GPowney <g.powney@googlemail.com>
2018-03-07 13:16:22 +0000

    Add example of using the PlotOcc function

---
+++ R/plot_A17_species_occupancy_ts.R
@@ -0,0 +1,17 @@
+###### plot A17 species annual occupancy time-series ######
+
+rm(list = ls()) # clear R
+
+### source in the plotting function ###
+source("R/PlotOcc.r")
+
+### Add article 17 species data ###
+PlotOcc(indata = "../data/model_runs/",
+        output_path = "../output/",
+        REGION_IN_Q = "psi.fs.r_SCOTLAND",
+        y_axis_choice = "variable",
+        cluster_run = "cirrus",
+        jasmin_min_year = 1970)
+
+
+
