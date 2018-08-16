2fa533890fb028923d08b5d8884008394e8d5dc8
GPowney <g.powney@googlemail.com>
2018-08-16 16:21:50 +0100

    Added documentation to the SampPost function

--- R/SampPost.R
+++ R/SampPost.R
@@ -1,4 +1,21 @@
-###### Sample posterior function ######
+#' SampPost - Sample posterior function
+#' 
+#' @description This function is used to extract a number of annual 
+#' occupancy estimates from any given posterior. The function loops 
+#' through Bugs output files within a given location.
+#'
+#' @param indata The file path to a location containing .rdata files
+#'      of the bugs outputs, one for each species
+#' @param output_path The location where the output will be saved.
+#' @param REGION_IN_Q The region for which occupancy estimates will be 
+#'      extracted. UK based occupancy model examples include,
+#'		  'psi.fs', 'psi.fs.r_GB', 'psi.fs.r_ENLGAND'.
+#' @param sample_n The number of values extracted from the posterior.
+#'
+#' @return A .csv file for each species containing annual occupancy 
+#'       estiamtes for each year as columns, and an iteration and species 
+#'       name column.
+#' @export
 
 SampPost <- function(indata = "../data/model_runs/", 
                     output_path = "../data/sampled_posterior_1000/",
